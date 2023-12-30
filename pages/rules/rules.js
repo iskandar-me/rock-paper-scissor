3fb9ada404ab53e7caef91e2bd5f0ca2331c9b06
Iskandar-tulkinov <iscandardev@gmail.com>
2023-12-31 00:36:42 +0500

    feat: add mobile media styles

--- pages/rules/rules.js
+++ pages/rules/rules.js
@@ -25,3 +25,23 @@ blocker.addEventListener('click',()=>{
   document.body.style.overflow='scroll'
 
 })
+
+// Get the open-rules-btn element
+const openRulesBtn = document.querySelector('.open-rules-btn');
+
+// Add an event listener for window resize
+window.addEventListener('resize', function() {
+  // Check the window width
+  if (window.innerWidth < 768) {
+    // If window width is less than 768px, add the "phone" class
+    openRulesBtn.classList.add('phone');
+  } else {
+    // If window width is 768px or more, remove the "phone" class
+    openRulesBtn.classList.remove('phone');
+  }
+});
+
+// Optionally, you can also check the initial window width on page load
+if (window.innerWidth < 768) {
+  openRulesBtn.classList.add('phone');
+}
